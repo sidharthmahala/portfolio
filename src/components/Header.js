9c75904ef106e9d8932a4b68d074db58a4069766
Sidharth Mahala <sidharthmahala@gmail.com>
2022-11-17 22:27:05 +0530

    responsive construction page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,48 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-//import { useState } from "react";
-//import {Router, Routes, Route} from 'react-router-dom'
+import { useState } from "react";
 import './Header.css'
 
 function Header() {
-  //const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
   return (
-    <div className="header--container">
-      <div className="logo--container">
-        <h1><NavLink to='/'>SIDHARTH MAHALA</NavLink></h1>
-      </div>
-      <nav className="nav--container">
-        <div className="nav--link">
-          <NavLink to='/'>Home</NavLink>
+    <div className="sticky-header">
+      <div className="header--container">
+        <div className="logo--container">
+          <h1><NavLink to='/'>SIDHARTH MAHALA</NavLink></h1>
         </div>
-        <div className="nav--link">
-          <NavLink to='/About'>About</NavLink>
-        </div>
-        <div className="nav--link">
-          <NavLink to='/Skills'>Skills</NavLink>
-        </div>
-        <div className="nav--link">
-          <NavLink to='/Projects'>Projects</NavLink>
-        </div>
-        <div className="nav--link">
-          {/* <a href='../Assets/ResumeSidharthm.pdf' target='_blank'>Resume</a> */}
-          <NavLink to='../Assets/ResumeSidharthm.pdf' target='_blank'>Resume</NavLink>
-        </div>
-        <div className="nav--link">
-          <NavLink to='/Blog'>Blog</NavLink>
-        </div>
-        <div className="nav--link"><NavLink to='/Contact'>
-          Contact
-        </NavLink>
-        </div>
-        {/* <button className="theme-toggle-btn">Light/Dark</button> */}
-      </nav>
-      <button className='hamburger--menu'>
+        <nav className={isNavExpanded ? "nav--container expanded" : "nav--conatainer"}>
+          <div className="nav--link">
+            <NavLink to='/'>Home</NavLink>
+          </div>
+          <div className="nav--link">
+            <NavLink to='/About'>About</NavLink>
+          </div>
+          <div className="nav--link">
+            <NavLink to='/Skills'>Skills</NavLink>
+          </div>
+          <div className="nav--link">
+            <NavLink to='/Projects'>Projects</NavLink>
+          </div>
+          <div className="nav--link">
+            {/* <a href='../Assets/ResumeSidharthm.pdf' target='_blank'>Resume</a> */}
+            <NavLink to='../Assets/ResumeSidharthm.pdf' target='_blank'>Resume</NavLink>
+          </div>
+          <div className="nav--link">
+            <NavLink to='/Blog'>Blog</NavLink>
+          </div>
+          <div className="nav--link"><NavLink to='/Contact'>
+            Contact
+          </NavLink>
+          </div>
+          {/* <button className="theme-toggle-btn">Light/Dark</button> */}
+        </nav>
+        <button className='hamburger--menu' onClick={() => { setIsNavExpanded(!isNavExpanded); }} >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
         </button>
+      </div>
     </div>
   )
 }
