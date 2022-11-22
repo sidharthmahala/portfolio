bb74a9e577f91eec8668caaf71e81f1b669747f8
Sidharth Mahala <sidharthmahala@gmail.com>
2022-11-22 16:37:42 +0530

    responsive error renoved skillsection

--- src/Pages/Home/SkillsSection.js
+++ src/Pages/Home/SkillsSection.js
@@ -3,68 +3,66 @@ import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNode, FaPython, FaGit, FaGithub } 
 import { SiTypescript, SiCanva, SiRedux, SiMongodb } from 'react-icons/si';
 import './SkillsSection.css'
 
+
 function SkillsSection() {
   return (
     <div>
       <div className='section-wrap'>
-        <div className='container'>
-        <div className='info-card'>
-          <h1>Skills</h1> 
-          <p>These are the skills I am Well versed in. </p>
-          <div className="btn-container">
-            
+        <div className='tscontainer'>
+          <div className='info-text'>
+            <h1>Skills</h1>
+            <p>These are the skills I am Well versed in. </p>
           </div>
-        </div>
-        <div className="skills-container">
-          <div className="icon-wrap">
-            <FaHtml5 className="skill-icon" />
-            <h3>HTML5</h3>
+          <div className="skills-container">
+            <div className="icon-wrap">
+              <FaHtml5 className="skill-icon" />
+              <h3>HTML5</h3>
+            </div>
+            <div className="icon-wrap">
+              <FaCss3Alt className="skill-icon" />
+              <h3>CSS3</h3>
+            </div>
+            <div className="icon-wrap">
+              <FaJs className="skill-icon" />
+              <h3>JavaScript</h3>
+            </div>
+            <div className="icon-wrap">
+              <FaReact className="skill-icon" />
+              <h3>Reactjs</h3>
+            </div>
+            <div className="icon-wrap">
+              <FaNode className="skill-icon" />
+              <h3>NodeJS</h3>
+            </div>
+            <div className="icon-wrap">
+              <SiTypescript className="skill-icon" />
+              <h3>Typescript</h3>
+            </div>
+            <div className="icon-wrap">
+              <SiRedux className="skill-icon" />
+              <h3>Redux</h3>
+            </div>
+            <div className="icon-wrap">
+              <FaGit className="skill-icon" />
+              <h3>Git</h3>
+            </div>
+            <div className="icon-wrap">
+              <FaGithub className="skill-icon" />
+              <h3>GitHub</h3>
+            </div>
+            <div className="icon-wrap">
+              <SiMongodb className="skill-icon" />
+              <h3>MongoDB</h3>
+            </div>
+            <div className="icon-wrap">
+              <FaPython className="skill-icon" />
+              <h3>Python</h3>
+            </div>
+            <div className="icon-wrap">
+              <SiCanva className="skill-icon" />
+              <h3>Canva</h3>
+            </div>
           </div>
-          <div className="icon-wrap">
-            <FaCss3Alt className="skill-icon" />
-            <h3>CSS3</h3>
-          </div>
-          <div className="icon-wrap">
-            <FaJs className="skill-icon" />
-            <h3>JavaScript</h3>
-          </div>
-          <div className="icon-wrap">
-            <FaReact className="skill-icon" />
-            <h3>Reactjs</h3>
-          </div>
-          <div className="icon-wrap">
-            <FaNode className="skill-icon" />
-            <h3>NodeJS</h3>
-          </div>
-          <div className="icon-wrap">
-            <SiTypescript className="skill-icon" />
-            <h3>Typescript</h3>
-          </div>
-          <div className="icon-wrap">
-            <SiRedux className="skill-icon" />
-            <h3>Redux</h3>
-          </div>
-          <div className="icon-wrap">
-            <FaGit className="skill-icon" />
-            <h3>Git</h3>
-          </div>
-          <div className="icon-wrap">
-            <FaGithub className="skill-icon" />
-            <h3>GitHub</h3>
-          </div>
-          <div className="icon-wrap">
-            <SiMongodb className="skill-icon" />
-            <h3>MongoDB</h3>
-          </div>
-          <div className="icon-wrap">
-            <FaPython className="skill-icon" />
-            <h3>Python</h3>
-          </div>
-          <div className="icon-wrap">
-            <SiCanva className="skill-icon" />
-            <h3>Canva</h3>
-          </div> 
-        </div>
         </div>
         <button className="btn">Skills</button>
       </div>
